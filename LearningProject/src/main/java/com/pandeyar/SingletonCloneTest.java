98d89046e0f58fa37886d4fedcce1987f918cc2a
Arun Pandey <colloquy84@gmail.com>
2025-04-18 18:02:26 +0530

    Added spring security configs

--- LearningProject/src/main/java/com/pandeyar/SingletonCloneTest.java
+++ LearningProject/src/main/java/com/pandeyar/SingletonCloneTest.java
@@ -1,39 +1,37 @@
 package com.pandeyar;
 
-public class SingletonCloneTest implements Cloneable{
+public class SingletonCloneTest implements Cloneable {
 
-	int aNumber;
+    int aNumber;
 
-	public static void main(String[] args) {
-		try {
-			SingletonCloneTest singletonCloneTest = new SingletonCloneTest();
-			singletonCloneTest.aNumber = 23;
+    public static void main(String[] args) {
+        try {
+            SingletonCloneTest singletonCloneTest = new SingletonCloneTest();
+            singletonCloneTest.aNumber = 23;
 
-			SingletonCloneTest singletonCloneTestClone = (SingletonCloneTest) singletonCloneTest
-					.clone();
-			
-			singletonCloneTestClone.aNumber=28;
-			
-			System.out.println("singletonCloneTest.aNumber is "+singletonCloneTest.aNumber);
-			System.out.println("singletonCloneTestClone.aNumber is "+singletonCloneTestClone.aNumber);
-			
-			System.out.println(singletonCloneTest);
-			System.out.println(singletonCloneTest.getClass().getName()+'@'+Integer.toHexString(singletonCloneTest.hashCode()));
-			
-			
-			
+            SingletonCloneTest singletonCloneTestClone = (SingletonCloneTest) singletonCloneTest
+                    .clone();
 
-		} catch (CloneNotSupportedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            singletonCloneTestClone.aNumber = 28;
 
-	}
-	
-	@Override
-	protected Object clone() throws CloneNotSupportedException {
-		// TODO Auto-generated method stub
-		return super.clone();
-	}
+            System.out.println("singletonCloneTest.aNumber is " + singletonCloneTest.aNumber);
+            System.out.println("singletonCloneTestClone.aNumber is " + singletonCloneTestClone.aNumber);
+
+            System.out.println(singletonCloneTest);
+            System.out.println(singletonCloneTest.getClass().getName() + '@' + Integer.toHexString(singletonCloneTest.hashCode()));
+
+
+        } catch (CloneNotSupportedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
 
 }
